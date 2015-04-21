0d803fc5953ff5118cb58614f184a2dcfda27010
Roshanjossey <roshanjossey@gmail.com>
2015-04-21 11:17:58 +0530

    First one finished

--- Prime in a range.py
+++ Prime in a range.py
@@ -1,28 +1,34 @@
+import math
 def primeinrange(rng):
   a = [True]*(rng + 1) #Make a boolean list with size range
   a[0] = a[1] = False # 0 and 1 are made composite
   # To make all even no.s except 2 composite. This decreases no. of iterations to half
   j = 4
-  while j < rng :
+  while j <= rng :
     a[j] = False
     j += 2
-  #Now numbers from 3 to spuareroot of range is checked if found as prime,all of its multiples are made composite
+  #Now numbers from 3 to squareroot of range is checked if found as prime,all of its multiples are made composite
   i = 3
-  while i < math.sqrt(rng):
-      if a[i] == False:
+  while i <= math.sqrt(rng):
+    if a[i] == True:
       j = i+i
-      while j < 2000001:
-        a[j] = i
+      while j <= rng:
+        a[j] = False
         j += i
-  i += 2
-#print(a)
-def primsum(c):
-  if a[c] == c:
-    return(c)
-  else:
-    return(a[c]+primsum(int(c/a[c])))
-tots = 0
-for x in range(int(input())):
-  n = int(input())
-  tots += primsum(n)
-print(int(tots))
+    i += 2
+  print('2',end=' ')
+  i = 3
+  while i<= rng:
+    if a[i] == True:
+      print(i,end = ' ')
+    i += 2
+  print('')
+def main():
+  print("Enter range:  ",end ='')
+  rng = int(input())
+  if rng < 2:
+    print('No prime numbers')
+    return(0)
+  primeinrange(rng)
+if __name__ == '__main__':
+  main()
